c75d7e394c1b392e0dc26227d36abc6b0089555a
Damian-Chowanski <dchowanski30@gmail.com>
2023-08-07 19:07:33 +0200

    new methods created

--- Zajecia12/src/Main.java
+++ Zajecia12/src/Main.java
@@ -1,8 +1,53 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Random;
+
 public class Main {
+    static int [][] randomNumbers = new int[10][10];
+    static int [] longList = new int[1000];
+    static Random rand = new Random();
+
     public static void main(String[] args) {
 
+        ganarateList();
+        System.out.println("Liczba 0 występuje: " + countOccurrences(0));
+        System.out.println("Liczba 13 występuje: " + countOccurrences(13));
+        System.out.println("Liczba 82 występuje: " + countOccurrences(82));
+        //generate();
+        //display();
+    }
 
+    private static int countOccurrences(int number) {
+        int sum = 0;
+        for (int j : longList) {
+            if (j == number) {
+                sum++;
+            }
+        }
+
+        return sum;
+    }
+
+    private static void ganarateList() {
+        for (int i = 0; i < longList.length; i++){
+            longList[i] = rand.nextInt(0,83);
+        }
+    }
+
+    private static void display() {
+        for (int[] randomNumber : randomNumbers) {
+            for (int j = 0; j < randomNumbers.length; j++) {
+                System.out.print(randomNumber[j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void generate() {
+
+        for (int i=0; i < randomNumbers.length; i++){
+            for(int j = 0; j < randomNumbers.length; j++){
+                randomNumbers[i][j] = rand.nextInt(-10,21);
+            }
+        }
     }
 }
