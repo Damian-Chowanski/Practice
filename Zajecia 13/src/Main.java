2c4388fecf6b94ad467c426c1755b25636c53d64
Damian-Chowanski <dchowanski30@gmail.com>
2023-08-09 18:16:25 +0200

    first task - two dim table with multpilication

--- Zajecia 13/src/Main.java
+++ Zajecia 13/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
+
+    static int [][] multiplication = new int [10][10];
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        generate();
+        print();
+    }
+
+    private static void print() {
+        for (int[] ints : multiplication) {
+            for (int j = 0; j < multiplication.length; j++) {
+                System.out.print(ints[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void generate() {
+        for (int i = 0; i<multiplication.length; i++){
+            for (int j=0; j< multiplication.length; j++){
+                multiplication[i][j] = (i+1)*(j+1);
+            }
+        }
     }
 }
