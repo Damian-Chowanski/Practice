75e7b9deeae4e85b927a78d098bf588a9f263322
Damian-Chowanski <dchowanski30@gmail.com>
2023-08-23 20:21:18 +0200

    task2 done

--- Zajecia 17/src/Main.java
+++ Zajecia 17/src/Main.java
@@ -1,12 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //zadanie1();
         zadanie2();
     }
 
-    private static void zadanie2() {
+    private static void zadanie2() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("ProtectedData.txt"));
+        String result = "";
+        while (sc.hasNextLine()) {
+            result += (char) Integer.parseInt(sc.nextLine());
 
+        }
+        System.out.println(result);
     }
+    //
+            /*if (x == 32){
+                newLineAfterSeven++;
+            }
+            if (enterAfterSeven == 7){
+                System.out.println();
+                newLineAfterSeven=0;
+            }*/
+
 
     private static void zadanie1() {
         for (int i = 0; i < 101; i++) {
