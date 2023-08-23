23888f14a6f509237a273d24746c9b826b6fa9be
Damian-Chowanski <dchowanski30@gmail.com>
2023-08-23 20:39:08 +0200

    homework done

--- Zajecia 17/src/Main.java
+++ Zajecia 17/src/Main.java
@@ -1,12 +1,45 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    static char [][] square = new char[7][7];
     public static void main(String[] args) throws FileNotFoundException {
         //zadanie1();
-        zadanie2();
+        //zadanie2();
+        zadanie3();
+        displayTab();
+    }
+
+    private static void displayTab() {
+        for (int i = 0; i < square.length; i++){
+            for (int j = 0; j < square.length; j++) {
+                if (square[i][j] != 'X') {
+                    System.out.print("-");
+                } else {
+                    System.out.print(square[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static void zadanie3() {
+        Random rnd = new Random();
+        int qty = rnd.nextInt(5,15);
+        for (int i = 0; i < qty;i++){
+            int x = rnd.nextInt(0,6);
+            int y = rnd.nextInt(0,6);
+            if(square[x][y] == 'X'){
+                i--;
+            } else {
+                square[x][y] = 'X';
+            }
+        }
+
+
     }
 
     private static void zadanie2() throws FileNotFoundException {
