ec4d5faacf865516ec39f2d4c7d63766f36aac14
Damian-Chowanski <dchowanski30@gmail.com>
2023-08-23 19:12:04 +0200

    task 1 done

--- Zajecia 17/src/Main.java
+++ Zajecia 17/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //zadanie1();
+        zadanie2();
+    }
+
+    private static void zadanie2() {
+
+    }
+
+    private static void zadanie1() {
+        for (int i = 0; i < 101; i++) {
+            if (i != 0 && i != 1) {
+                int dividers = 0;
+                for (int j = 1; j < i; j++) {
+                    if (i % j == 0) {
+                        dividers++;
+                    }
+                }
+                if (dividers == 1) {
+                    System.out.println(i);
+                }
+            }
+        }
     }
 }
