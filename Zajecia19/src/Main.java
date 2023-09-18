9a2b4d594939cd86c5fe16cc651bb4bf44af94c9
Damian-Chowanski <dchowanski30@gmail.com>
2023-09-18 19:44:39 +0200

    checking population by month created

--- Zajecia19/src/Main.java
+++ Zajecia19/src/Main.java
@@ -1,5 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    static ArrayList<String> peselNumbers = new ArrayList<>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner readFile = new Scanner(new File("pesel.txt"));
+        Scanner sc = new Scanner(System.in);
+
+        readFileToList(readFile);
+        System.out.print("Podaj swój miesiąc urodzenia w formacie dwóch cyfr [01-12]: ");
+        String month = sc.next();
+
+        checkPopulationInSelectedMonth(month);
+
+    }
+
+    private static void checkPopulationInSelectedMonth(String month) {
+        int women = 0;
+        int men = 0;
+
+        for (String pesel : peselNumbers) {
+            if (pesel.substring(2, 4).equals(month)) {
+                if (pesel.charAt((pesel.length()-1)) % 2 == 0) {
+                    women++;
+                } else men++;
+            }
+        }
+        System.out.println("W twoim miesiącu urodziło się " + women + " kobiet i " + men + " mężczyżn.");
+    }
+
+    private static void readFileToList(Scanner sc) {
+
+        while (sc.hasNextLine()) {
+            peselNumbers.add(sc.nextLine());
+        }
     }
 }
