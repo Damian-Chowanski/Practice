fb930abb4255ca371bb8b2009d541041f947184c
Damian-Chowanski <dchowanski30@gmail.com>
2023-09-20 18:20:17 +0200

    Student class created. Subject class blueprint

--- Zajecia19/src/BestStudents.java
+++ Zajecia19/src/BestStudents.java
@@ -5,13 +5,22 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class BestStudents {
+
+    static ArrayList<Student> listOfStudents = new ArrayList<>();
     public static void main(String[] args) throws FileNotFoundException {
         Scanner studentsFile = new Scanner(new File("uczniowie.txt"));
         Scanner markFile = new Scanner(new File("oceny.txt"));
 
+        loadStudentsToList(studentsFile);
 
 
+    }
 
-
+    private static void loadStudentsToList(Scanner scanner) {
+        scanner.nextLine();
+        while (scanner.hasNextLine()){
+            String[] currentLine = scanner.nextLine().split(";");
+            listOfStudents.add(new Student(Integer.parseInt(currentLine[0]),currentLine[1],currentLine[2],currentLine[3],Integer.parseInt(currentLine[4]),currentLine[5]));
+        }
     }
 }
--- Zajecia19/src/Student.java
+++ Zajecia19/src/Student.java
@@ -1,2 +1,77 @@
 public class Student {
+    private int ID;
+    private String lastname;
+    private String name;
+    private String street;
+    private int home;
+    private String classID;
+
+    public Student(int ID, String lastname, String name, String street, int home, String classID) {
+        this.ID = ID;
+        this.lastname = lastname;
+        this.name = name;
+        this.street = street;
+        this.home = home;
+        this.classID = classID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHome() {
+        return home;
+    }
+
+    public void setHome(int home) {
+        this.home = home;
+    }
+
+    public String getClassID() {
+        return classID;
+    }
+
+    public void setClassID(String classID) {
+        this.classID = classID;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "ID=" + ID +
+                ", lastname='" + lastname + '\'' +
+                ", name='" + name + '\'' +
+                ", street='" + street + '\'' +
+                ", home=" + home +
+                ", classID='" + classID + '\'' +
+                '}';
+    }
 }
