141d836f69be8fd012a5635bc18ac51b03e63017
Damian-Chowanski <dchowanski30@gmail.com>
2023-09-20 17:50:34 +0200

    new task started, created Student class

---
+++ Zajecia19/src/BestStudents.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BestStudents {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner studentsFile = new Scanner(new File("uczniowie.txt"));
+        Scanner markFile = new Scanner(new File("oceny.txt"));
+
+
+
+
+
+    }
+}
